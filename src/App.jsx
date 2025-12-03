64b3dcd97283ecd6a52bcd3a0c4befa976d7ceec
murphy-afk <bardarenoemi@gmail.com>
2025-12-03 15:01:48 +0100

    feat: api call

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useEffectEvent, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [actresses, setActresses] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect is happening");
+    fetchData()
+  }, []);
   
+  function fetchData() {
+    axios .get("https://lanciweb.github.io/demo/api/actresses/")
+    .then((resp) => {
+      console.log("fetchData is happening");
+      setActresses(resp.data);
+      console.log(resp.data);
+      
+    })
+  }
 
   return (
     <>
