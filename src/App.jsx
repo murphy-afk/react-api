02bbfc7533155002b4b03936e3eea833858e0b7e
murphy-afk <bardarenoemi@gmail.com>
2025-12-03 15:19:23 +0100

    feat: actress card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useEffectEvent, useState } from 'react'
 import './App.css'
 import axios from 'axios'
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
   const [actresses, setActresses] = useState([]);
@@ -9,20 +10,40 @@ function App() {
     console.log("useEffect is happening");
     fetchData()
   }, []);
-  
+
   function fetchData() {
-    axios .get("https://lanciweb.github.io/demo/api/actresses/")
-    .then((resp) => {
-      console.log("fetchData is happening");
-      setActresses(resp.data);
-      console.log(resp.data);
-      
-    })
+    axios.get("https://lanciweb.github.io/demo/api/actresses/")
+      .then((resp) => {
+        console.log("fetchData is happening");
+        setActresses(resp.data);
+      })
   }
 
   return (
     <>
-    <h1>prova</h1>
+      <h1>Actresses</h1>
+      <div className="container">
+        <div className="row row-cols-3">
+          {actresses.map(actress =>
+            <div className="card col" key={actress.id}>
+              <img src={actress.image} className="card-img-top" alt={actress.name} />
+              <div className="card-body">
+                <h5 className="card-title">{actress.name}</h5>
+                <p className='card-subtitle'>{actress.nationality}</p>
+                <p className='card-subtitle'>{actress.birth_year}</p>
+                <p className="card-text">{actress.biography}</p>
+                <div>Most Known for:
+                  <ul>
+                    {actress.most_famous_movies.map((movie, index) =>
+                      <li key={index}>{movie}</li>
+                    )}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
     </>
   )
 }
